bcda866066bfa57f5b9cab9229f929eeed173257
BY <49119921+gitthehit@users.noreply.github.com>
2023-02-14 16:56:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app } = require('electron');
-app.commandLine.appendSwitch('proxy-server', 'socks5://127.0.0.1:10808');\\you can change ip:port to your own proxy address
-\\app.commandLine.appendSwitch('proxy-bypass-list', '<local>;*.baidu.com');
+app.commandLine.appendSwitch('proxy-server', 'socks5://127.0.0.1:10808');//you can change ip:port to your own proxy address
+//app.commandLine.appendSwitch('proxy-bypass-list', '<local>;*.baidu.com');
 class netproxy {
     name = 'net proxy setting';
     description = 'set Cider use given proxy address'
@@ -12,16 +12,16 @@ class netproxy {
   
     constructor(env) {
         this.env = env
-        \\this.debug('Loading Complete')
+        //this.debug('Loading Complete')
     }
   
     async onReady(win) {
         this.win = win
-        \\this.debug('Ready')
+        //this.debug('Ready')
     }
   
     onBeforeQuit() {
-        \\this.debug('Stopped')
+        //this.debug('Stopped')
     }
   
     onPlaybackStateDidChange(attributes) {
