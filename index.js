c7e77620b5639040b901dc7794210340f621cc9b
BY <49119921+gitthehit@users.noreply.github.com>
2023-02-14 16:18:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-const { app } = require('electron')
-app.commandLine.appendSwitch('proxy-server', '127.0.0.1:10808')
+const { app } = require('electron');
+app.commandLine.appendSwitch('proxy-server', 'socks5://127.0.0.1:10808');
+app.commandLine.appendSwitch('proxy-bypass-list', '<local>;*.baidu.com');
+class netproxy {
+    name = 'net proxy setting';
+    description = 'set Cider use given proxy address'
+    version = '1.0.0';
+    author = 'BY';
+  
+    env
+    win
+  
+    constructor(env) {
+        this.env = env
+        this.debug('Loading Complete')
+    }
+  
+    async onReady(win) {
+        this.win = win
+        this.debug('Ready')
+    }
+  
+    onBeforeQuit() {
+        this.debug('Stopped')
+    }
+  
+    onPlaybackStateDidChange(attributes) {
+      
+    }
+    
+    onNowPlayingItemDidChange(attributes) {
+      
+    }
+    
+    debug(text) {
+        console.log(`[Plugin][${this.name}]`, text)
+    }
+}
+
+module.exports = netproxy;
